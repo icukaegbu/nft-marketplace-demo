fe007ba716e400e6d8c77ce38b0b6e1b2fc77270
Chibueze <icukaegbu@gmail.com>
2023-03-07 16:02:26 +0100

    Demo data

---
+++ src/Utils/data.ts
@@ -0,0 +1,62 @@
+export const NFTS = [
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197937/misc/7748166_i79owt.jpg",
+    title: "Bored Ape: 1",
+    category: "Checkers"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7748178_mtmeki.jpg",
+    title: "Bored Ape: 2",
+    category: "Metaverse"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763251_v2q4oj.jpg",
+    title: "Bored Ape: 3",
+    category: "Metaverse"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763260_eidscn.jpg",
+    title: "Bored Ape: 4",
+    category: "Gaming"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763244_pbe9jx.jpg",
+    title: "Bored Ape: 5",
+    category: "Checkers"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763238_vntza7.jpg",
+    title: "Bored Ape: 6",
+    category: "Gaming"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7742244_hybg7h.jpg",
+    title: "Bored Ape: 7",
+    category: "Metaverse"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7704887_asyztg.jpg",
+    title: "Bored Ape: 8",
+    category: "Gaming"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7705305_f2tvoz.jpg",
+    title: "Bored Ape: 9",
+    category: "Gaming"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7705329_kur5h6.jpg",
+    title: "Bored Ape: 10",
+    category: "Checkers"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7705323_fv6jv8.jpg",
+    title: "Bored Ape: 11",
+    category: "Gaming"
+  },
+  {
+    image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7742218_nccbuj.jpg",
+    title: "Bored Ape: 12",
+    category: "Metaverse"
+  },
+]
