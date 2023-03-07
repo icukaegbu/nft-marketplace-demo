c8646ea5e239882597d252063d4d9fade5a22917
Chibueze <icukaegbu@gmail.com>
2023-03-07 16:03:07 +0100

    CardList component

---
+++ src/Components/CardList/CardList.tsx
@@ -0,0 +1,21 @@
+import { Container, Grid  } from "@mantine/core"
+import NFTHeroCard from "../NFTHeroCard/NFTHeroCard"
+import { NFTS } from "../../Utils/data"
+
+function CardList() {
+  return (
+    <Container size="xl" px="xl">
+      <Grid justify="space-around" gutter={5} gutterXs="md" gutterMd="xl" gutterXl={40}>
+        {
+          NFTS.map(({ image, category, title }) => (
+            <Grid.Col style={{ maxWidth: "350px"}} sm={4} xs={4}>
+              <NFTHeroCard image={image} category={category} title={title} />
+            </Grid.Col>
+          ))
+        }
+      </Grid>
+    </Container>
+  )
+}
+
+export default CardList
