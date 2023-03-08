1e79477d5c67adf03b6e9b3e771a5a382192cbcd
Chibueze <icukaegbu@gmail.com>
2023-03-08 13:44:31 +0100

    Updated demo data

--- src/Utils/data.ts
+++ src/Utils/data.ts
@@ -2,61 +2,117 @@ export const NFTS = [
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197937/misc/7748166_i79owt.jpg",
     title: "Bored Ape: 1",
-    category: "Checkers"
+    category: "Checkers",
+    owner: "Nkem Obasi",
+    ownerAddress: "1 Asa Road",
+    description: "Lorem ipsum is a great way to know",
+    price: 500,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7748178_mtmeki.jpg",
     title: "Bored Ape: 2",
-    category: "Metaverse"
+    category: "Metaverse",
+    owner: "Chidu Ukaegbu",
+    ownerAddress: "3 Kilimanjaro Street",
+    description: "Hallo dmonius ",
+    price: 2210,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763251_v2q4oj.jpg",
     title: "Bored Ape: 3",
-    category: "Metaverse"
+    category: "Metaverse",
+    owner: "Chidu Ukaegbu",
+    ownerAddress: "3 Kilimanjaro Street",
+    description: "Hallo dmonius ",
+    price: 5560,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763260_eidscn.jpg",
     title: "Bored Ape: 4",
-    category: "Gaming"
+    category: "Gaming",
+    owner: "Nkem Obasi",
+    ownerAddress: "1 Asa Road",
+    description: "Lorem ipsum is a great way to know",
+    price: 4600,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763244_pbe9jx.jpg",
     title: "Bored Ape: 5",
-    category: "Checkers"
+    category: "Checkers",
+    owner: "Chidu Ukaegbu",
+    ownerAddress: "3 Kilimanjaro Street",
+    description: "Hallo dmonius ",
+    price: 2110,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197936/misc/7763238_vntza7.jpg",
     title: "Bored Ape: 6",
-    category: "Gaming"
+    category: "Gaming",
+    owner: "Chidu Ukaegbu",
+    ownerAddress: "3 Kilimanjaro Street",
+    description: "Hallo dmonius ",
+    price: 650,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7742244_hybg7h.jpg",
     title: "Bored Ape: 7",
-    category: "Metaverse"
+    category: "Metaverse",
+    owner: "Nkem Obasi",
+    ownerAddress: "1 Asa Road",
+    description: "Lorem ipsum is a great way to know",
+    price: 2500,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7704887_asyztg.jpg",
     title: "Bored Ape: 8",
-    category: "Gaming"
+    category: "Gaming",
+    owner: "Chidi Mokeme",
+    ownerAddress: "Magodo Estate, Lagos",
+    description: "Sunil is the best of them all",
+    price: 4560,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7705305_f2tvoz.jpg",
     title: "Bored Ape: 9",
-    category: "Gaming"
+    category: "Gaming",
+    owner: "Chidu Ukaegbu",
+    ownerAddress: "3 Kilimanjaro Street",
+    description: "Hallo dmonius ",
+    price: 1150,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7705329_kur5h6.jpg",
     title: "Bored Ape: 10",
-    category: "Checkers"
+    category: "Checkers",
+    owner: "Nkem Obasi",
+    ownerAddress: "1 Asa Road",
+    description: "Sancta maria avensis",
+    price: 1500,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7705323_fv6jv8.jpg",
     title: "Bored Ape: 11",
-    category: "Gaming"
+    category: "Gaming",
+    owner: "Chidi Mokeme",
+    ownerAddress: "Magodo Estate, Lagos",
+    description: "Sunil is the best of them all",
+    price: 2280,
   },
   {
     image: "https://res.cloudinary.com/dkpiok7n3/image/upload/v1678197935/misc/7742218_nccbuj.jpg",
     title: "Bored Ape: 12",
-    category: "Metaverse"
+    category: "Metaverse",
+    owner: "Chidi Mokeme",
+    ownerAddress: "Magodo Estate, Lagos",
+    description: "Larger than life",
+    price: 3345,
   },
+]
+
+
+export const FOOTER_LINKS = [
+  {
+    title: "",
+    links: [{ label: "", link: "" }, { label: "", link: "" }, { label: "", link: "" }, { label: "", link: "" }]
+  }
 ]
