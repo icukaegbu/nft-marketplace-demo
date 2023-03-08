5cf0add9a567857d363d618916afcc912a8fb8ed
Chibueze <icukaegbu@gmail.com>
2023-03-08 10:26:49 +0100

    Replace HeroCard with Modal as primary component

--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,15 +1,37 @@
+import { useState, useEffect } from "react"
 import { Container, Grid  } from "@mantine/core"
-import NFTHeroCard from "../NFTHeroCard/NFTHeroCard"
 import { NFTS } from "../../Utils/data"
+import { fetchNFTCollection, fetchOwnCollection } from "../../Utils/fetchNFTs"
+import { fetchNFTMoralis } from "../../Utils/fetchNFTMoralis"
+import NFTModal from "../Modal/NFTModal"
+// import NFTHeroCard from "../NFTHeroCard/NFTHeroCard"
 
 function CardList() {
+  const [nfts, setNFTs] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [address, setAddress] = useState("0xaAdBA140Ae5e4c8a9eF0Cc86EA3124b446e3E46A")
+
+  //fetch
+  useEffect(() => {
+    // fetchNFTCollection(address)
+    // fetchNFTMoralis()
+    fetchOwnCollection()
+    .then(data => {
+      console.log("getting data")
+      // setNFTs(data)
+      setIsLoading(false)
+      console.log(data)
+    })
+    .catch(err => setNFTs([]))
+  }, [address])
+
   return (
     <Container size="xl" px="xl">
       <Grid justify="space-around" gutter={5} gutterXs="md" gutterMd="xl" gutterXl={40}>
         {
-          NFTS.map(({ image, category, title }) => (
-            <Grid.Col style={{ maxWidth: "350px"}} sm={4} xs={4} key={image}>
-              <NFTHeroCard image={image} category={category} title={title} />
+          NFTS.map((card) => (
+            <Grid.Col style={{ maxWidth: "350px"}} sm={4} xs={4} key={card.image}>
+              <NFTModal card={card} />
             </Grid.Col>
           ))
         }
