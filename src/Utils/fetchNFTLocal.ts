6a3833313f814447684ed07a30287e7fab849c9e
Chibueze <icukaegbu@gmail.com>
2023-03-08 20:01:11 +0100

    Methods to fetch local NFT data

---
+++ src/Utils/fetchNFTLocal.ts
@@ -0,0 +1,14 @@
+import rings from "../Data/6rings.json"
+import anon from "../Data/888_anon_club.json"
+import akari from "../Data/akari.json"
+import curedlabs from "../Data/curedlabs.json"
+import doge from "../Data/doge_track.json"
+import kamakura from "../Data/kamakura.json"
+
+
+export const fetchRings = async () => rings
+export const fetchAnon = async () => anon
+export const fetchAkari = async () => akari
+export const fetchCuredLabs = async () => curedlabs
+export const fetchDoge = async () => doge
+export const fetchKamakura = async () => kamakura
