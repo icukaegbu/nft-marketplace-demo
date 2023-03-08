b058e396b8385094d8baad1cfc2e312864d36374
Chibueze <icukaegbu@gmail.com>
2023-03-08 11:06:01 +0100

    Adde footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,55 @@
-import { Stack, AppShell, Title, Header, Paper, Loader, Text } from "@mantine/core";
-import { ThemeProvider } from "./ThemeProvider";
+import {
+  Stack,
+  AppShell,
+  Title,
+  Header,
+  Paper,
+  Loader,
+  Text,
+} from '@mantine/core'
+import { ThemeProvider } from './ThemeProvider'
 
-import CardList from "./Components/CardList/CardList"
-import LightDarkButton from "./Components/LightDarkButton";
-import Buttons from "./Components/Buttons";
-import { Hero } from "./Components/Hero/Hero";
+import CardList from './Components/CardList/CardList'
+import LightDarkButton from './Components/LightDarkButton'
+import Buttons from './Components/Buttons'
+import { Hero } from './Components/Hero/Hero'
+import { FooterLinks } from './Components/Footer/Footer'
+import { FOOTER_LINKS } from './Utils/data'
 
 export default function App() {
   return (
     <ThemeProvider>
       <AppShell
-      padding="md"
-      // navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
-      header={<Header height={60} p="xs"><LightDarkButton /></Header>}
-      styles={(theme) => ({
-        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-      })}
-    >
-      <Hero />
-      <Stack align="center" mt={50}>
-        <Title 
-          order={1}
-          variant="gradient"
-          gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
-          sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
-        >
-          Trending NFTs
-        </Title>
-        {/* <Text
+        padding="md"
+        // navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
+        header={
+          <Header height={60} p="xs">
+            <LightDarkButton />
+          </Header>
+        }
+        footer={
+          <FooterLinks data={FOOTER_LINKS} />
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Hero />
+        <Stack align="center" mt={50}>
+          <Title
+            order={1}
+            variant="gradient"
+            gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+            sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+          >
+            Trending NFTs
+          </Title>
+          {/* <Text
           variant="gradient"
           gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
           sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
@@ -37,9 +59,9 @@ export default function App() {
         >
           Trending NFTs
         </Text> */}
-        <CardList />
-      </Stack>
+          <CardList />
+        </Stack>
       </AppShell>
     </ThemeProvider>
-  );
+  )
 }
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,63 @@
+import { Text, Container, ActionIcon, Group } from '@mantine/core';
+import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
+
+import { useStyles } from './Footer.style';
+
+import { FooterLinksProps } from '../../Utils/types';
+
+
+export function FooterLinks({ data }: FooterLinksProps) {
+  const { classes } = useStyles();
+
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text<'a'>
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          {/* <MantineLogo size={30} /> */}
+          <Text size="xs" color="dimmed" className={classes.description}>
+            Build fully functional accessible Web3 applications faster than ever
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text color="dimmed" size="sm">
+          © 2023 AlturaNFT. All rights reserved.
+        </Text>
+
+        <Group spacing={0} className={classes.social} position="right" noWrap>
+          <ActionIcon size="lg">
+            <IconBrandTwitter size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <IconBrandYoutube size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <IconBrandInstagram size="1.05rem" stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+}
